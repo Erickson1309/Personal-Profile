3dd5f7ed1665f16f695f449e1173f42435861746
Erickson Payumo <joelerickson@hotmail.co.uk>
2021-05-11 20:50:58 +0100

    fixing up navbar

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ window.addEventListener("scroll", function ()
   // console.log('offset * 0.7' + offset * 0.7)
   parallax.style.backgroundPositionY = offset * 0.1 + "px";
 })
+
+const parallax2 = document.getElementById
+("parallax2");
+
+window.addEventListener("scroll", function ()
+{
+  let offset = window.pageYOffset;
+  // console.log('Offset: ' + offset);
+  // console.log('offset * 0.7' + offset * 0.7)
+  parallax2.style.backgroundPositionY = offset * 0.3 + "px";
+})
