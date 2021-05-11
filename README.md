5255605d7dcbf80894587ddfe78b3344e5c9efcb
Erickson Payumo <joelerickson@hotmail.co.uk>
2021-05-11 23:24:14 +0100

    added navbar again. Added div boxes to block 2. Tried some fonts. Put flex box in block 2

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Personal-Profile
 Personal Profile Page
+
+Fonts:
+  font-family: 'Jost', sans-serif;
+  font-family: 'Yellowtail', cursive;
+  font-family: 'Amatic SC', cursive;
+  font-family: 'LiErbre Barcode 128 Text', cursive;
+  font-family: 'Poiret One', cursive;
+  font-family: 'Pompiere', cursive;
+  font-family: 'Spartan', sans-serif;
+  font-family: 'Voltaire', sans-serif;
--- main.js
+++ main.js
@@ -9,13 +9,13 @@ window.addEventListener("scroll", function ()
   parallax.style.backgroundPositionY = offset * 0.1 + "px";
 })
 
-const parallax2 = document.getElementById
-("parallax2");
+// const parallax2 = document.getElementById
+// ("parallax2");
 
-window.addEventListener("scroll", function ()
-{
-  let offset = window.pageYOffset;
-  // console.log('Offset: ' + offset);
-  // console.log('offset * 0.7' + offset * 0.7)
-  parallax2.style.backgroundPositionY = offset * 0.3 + "px";
-})
+// window.addEventListener("scroll", function ()
+// {
+//   let offset = window.pageYOffset;
+//   // console.log('Offset: ' + offset);
+//   // console.log('offset * 0.7' + offset * 0.7)
+//   parallax2.style.backgroundPositionY = offset * 1 + "px";
+// })
